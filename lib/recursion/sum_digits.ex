7f44ef78a92636e35d18c7a035e7311b29e28f9d
Tarmo <liivamagi.tarmo@gmail.com>
2024-05-27 15:36:10 +0300

    more examples

--- lib/recursion/sum_digits.ex
+++ lib/recursion/sum_digits.ex
@@ -6,9 +6,9 @@ defmodule Tutorials.Recursion.SumDigits do
   def upto(num) do
     upto_tail_rec(num)
   end
-  def upto_tail_rec(num, accumulator \\ 0) do
-    accumulator
-  end
+  def upto_tail_rec(num, accumulator \\ 0)
+  def upto_tail_rec(0, acc), do: acc
+
   def upto_tail_rec(num, accumulator) do
     upto_tail_rec(num-1, accumulator + num)
   end
